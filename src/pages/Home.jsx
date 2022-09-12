cc84154ac358b6796090fafb4c8a8c9831511bb9
Puiu Crisan Bucur <puiu@bucur.info>
2022-09-12 14:19:48 +0300

    51 display users

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import UserResults from '../components/users/UserResults';
+function Home() {
+  return (
+    <>
+      {/* search component goes here */}
+      <UserResults />
+    </>
+  );
+}
+
+export default Home;
